de56ac1b9735fb8af5656c53009c1be5b9814226
m.zabic <m.zabic@lzh.de>
2019-09-13 13:14:34 +0200

    event guard added for better scrolling experience

---
+++ src/eventguard.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+**                                                                        **
+**  This file is part of FDMLControl.                                     **
+**  FDMLControl enables the control of the Optores FDML laser via a       **
+**  serial connection.                                                    **
+**  Copyright (C) 2019 Miroslav Zabic                                     **
+**                                                                        **
+**  FDMLControl is free software: you can redistribute it and/or modify   **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program. If not, see http://www.gnu.org/licenses/.    **
+**                                                                        **
+****************************************************************************
+**          Author:     Miroslav Zabic                                    **
+**          Contact:    zabic                                             **
+**                      at                                                **
+**                      iqo.uni-hannover.de                               **
+**                                                                        **
+****************************************************************************/
+
+#pragma once
+
+#include <QWidget>
+#include <QEvent>
+
+class EventGuard : public QObject
+{
+    Q_OBJECT
+public:
+    explicit EventGuard(QObject *parent);
+
+
+protected:
+    bool eventFilter(QObject* object, QEvent* event) override;
+
+
+signals:
+
+
+public slots:
+};
